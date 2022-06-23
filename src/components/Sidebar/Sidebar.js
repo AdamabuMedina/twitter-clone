d8a70874d8f1d07b6e00852a8375dcc75d2dd1d3
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2022-06-23 11:49:50 +0300

    инициализация приложение, верстка sidebarOption

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Sidebar.css"
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SidebarOption from './SidebarOption/SidebarOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <TwitterIcon />
+      <SidebarOption Icon={HomeIcon} text="Главная" />
+      <SidebarOption Icon={SearchIcon} text="Обзор" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Уведомления" />
+      <SidebarOption Icon={MailOutlineIcon} text="Сообщения" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Закладки" />
+      <SidebarOption Icon={ListAltIcon} text="Списки" />
+      <SidebarOption Icon={PermIdentityIcon} text="Профиль" />
+      <SidebarOption Icon={MoreHorizIcon} text="Еще" />
+    </div>
+  )
+}
+
+export default Sidebar
